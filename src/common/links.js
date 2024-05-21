c7abb356f8b5edd24f6b4afa346fb5407ecf56ec
shanmugamsanthosh22 <shanmugamsanthosh22@gmail.com>
2024-05-21 04:39:56 +0000

    vhvhvfsd

--- src/common/links.js
+++ src/common/links.js
@@ -2,10 +2,22 @@ export const ParseDate = (date) => {
   const event = new Date(date);
   const currentDate = new Date();
 
-  const timeDiffInMilliseconds = currentDate.getTime() - event.getTime();
-  const timeDiffInHours = Math.floor(timeDiffInMilliseconds / (1000 * 60 * 60));
+  // Calculate yesterday's date
+  const yesterday = new Date(currentDate);
+  yesterday.setDate(currentDate.getDate() - 1);
 
-  if (timeDiffInHours < 24) {
+  // Check if the event date is today
+  const isToday = event.getDate() === currentDate.getDate() &&
+                  event.getMonth() === currentDate.getMonth() &&
+                  event.getFullYear() === currentDate.getFullYear();
+
+  // Check if the event date is yesterday
+  const isYesterday = event.getDate() === yesterday.getDate() &&
+                    event.getMonth() === yesterday.getMonth() &&
+                    event.getFullYear() === yesterday.getFullYear();
+
+  if (isToday) {
+    // Display time if today
     const hours = event.getHours();
     const minutes = event.getMinutes();
     let period = "AM";
@@ -18,7 +30,22 @@ export const ParseDate = (date) => {
     const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
     return `${displayHours}:${displayMinutes} ${period}`;
+  } else if (isYesterday) {
+    // Display time if yesterday
+    const hours = event.getHours();
+    const minutes = event.getMinutes();
+    let period = "AM";
+
+    if (hours >= 12) {
+      period = "PM";
+    }
+
+    const displayHours = hours % 12 || 12;
+    const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `yesterday  ,${displayHours}:${displayMinutes} ${period} `;
   } else {
+    // Display the other format if not today or yesterday
     const months = [
       "Jan",
       "Feb",
@@ -35,10 +62,20 @@ export const ParseDate = (date) => {
     ];
     const monthIndex = event.getMonth();
     const month = months[monthIndex];
+    const hours = event.getHours();
+    const minutes = event.getMinutes();
+    let period = "AM";
+
+    if (hours >= 12) {
+      period = "PM";
+    }
+
+    const displayHours = hours % 12 || 12;
+    const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
     const day = event.getDate();
     const year = event.getFullYear();
 
-    return `${month} ${day}, ${year}`;
+    return `${displayHours}:${displayMinutes} ${period} , ${month} ${day}`;
   }
 };
