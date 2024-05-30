cbcd11a68ca6417536a33bff25dc3b80172d7282
Santhosh20112003 <shanmugamsanthosh22@gmail.com>
2024-05-31 01:26:08 +0530

    Mission3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import Jarvis1 from './Jarvis1';
 import Jarvis2 from './Jarvis2';
+
+function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const currentVersion = localStorage.getItem("current_version");
+    if (currentVersion === "1.0") {
+      navigate('/v1');
+    } else {
+      navigate('/v2');
+    }
+  }, [navigate]);
+
+  return null; 
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" index element={<Navigate to="v1" />} />
-        <Route path="v1" element={<Jarvis1 />} />
-        <Route path="v2" element={<Jarvis2 />} />
+        <Route path="" element={<Home />} />
+        <Route path="/v1" element={<Jarvis1 />} />
+        <Route path="/v2" element={<Jarvis2 />} />
       </Routes>
     </BrowserRouter>
   );
