217ee4f4339f9e12b0b01bc88a8dd354284d4277
Santhosh20112003 <shanmugamsanthosh22@gmail.com>
2024-05-31 00:31:21 +0530

    sdvsgdvgsvdg

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Jarvis1 from './Jarvis1';
 import Jarvis2 from './Jarvis2';
-
-function Home() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const currentVersion = localStorage.getItem("current_version");
-    if (currentVersion === "1.0") {
-      navigate('/version1.0');
-    } else {
-      navigate('/version2.0');
-    }
-  }, [navigate]);
-
-  return null; 
-}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Navigate to="/version2.0" />} />
         <Route path="/version1.0" element={<Jarvis1 />} />
         <Route path="/version2.0" element={<Jarvis2 />} />
       </Routes>
