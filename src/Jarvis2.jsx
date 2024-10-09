9681f98a69476dda0b7a3114b7a92df16f76f448
Santhosh20112003 <shanmugamsanthosh22@gmail.com>
2024-10-09 10:46:57 +0530

    QR code

--- src/Jarvis2.jsx
+++ src/Jarvis2.jsx
@@ -42,7 +42,7 @@ const model = genAI.getGenerativeModel({
 const generationConfig = {
   temperature: 1,
   topP: 0.95,
-  topK: 64,
+  topK: 40,
   maxOutputTokens: 8192,
   responseMimeType: "text/plain",
 };
